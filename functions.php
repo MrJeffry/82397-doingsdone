d4a25b174ff4764a2fdb89fa35538703744bfda5
Artem Altigin <aaltigin@gmail.com>
2018-05-13 13:38:17 +0300

    Поправил функцию шаблонизатор

--- functions.php
+++ functions.php
@@ -1,14 +1,14 @@
 <?php
     function input_validation($input) {
-        return $input = htmlspecialchars($input);
+        return htmlspecialchars($input);
     };
 
     function generate_template($template_path, $data) {
-        if (file_exists($template_path) and (isset($data))) {
+        if (file_exists($template_path) && (isset($data))) {
             ob_start();
             extract($data);
-            $print_data = require_once($template_path);
-            return $print_data = ob_get_clean();
+            require_once($template_path);
+            return ob_get_clean();
         };
-        print ('');
+        return '';
     };
