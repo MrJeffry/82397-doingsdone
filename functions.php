5ba014f99ca572a5dbd2fad732a2e0c601c806a7
Artem Altigin <aaltigin@gmail.com>
2018-05-13 14:00:10 +0300

    Правки

--- functions.php
+++ functions.php
@@ -3,12 +3,20 @@
         return htmlspecialchars($input);
     };
 
-    function generate_template($template_path, $data) {
-        if (file_exists($template_path) && (isset($data))) {
-            ob_start();
-            extract($data);
-            require_once($template_path);
-            return ob_get_clean();
+    /**
+     * @param string $template_name - название шаблона
+     * @param array $data - данные бадлона
+     * @return string
+     */
+    function generate_template(stirng $template_name, array $data) {
+        $template_path = TEMPLATE_PATH . $template_name . TEMPLATE_EXT;
+
+        if (!file_exists($template_path) && (!isset($data))) {
+            return '';
         };
-        return '';
+
+        ob_start();
+        extract($data);
+        require_once($template_path);
+        return ob_get_clean();
     };
--- index.php
+++ index.php
@@ -1,8 +1,11 @@
 <?php
+    define('TEMPLATE_PATH', './templates/');
+    define('TEMPLATE_EXT', '.php');
+
     require_once('./functions.php');
     require_once('./data.php');
 
-    $content = generate_template('./templates/index.php', ['tasks' => $tasks,'show_complete_tasks' => $show_complete_tasks]);
-    $layout = generate_template('./templates/layout.php', ['categories' => $categories, 'tasks' => $tasks, 'content' => $content, 'title' => $title]);
+    $content = generate_template('index', ['tasks' => $tasks,'show_complete_tasks' => $show_complete_tasks]);
+    $layout = generate_template('layout', ['categories' => $categories, 'tasks' => $tasks, 'content' => $content, 'title' => $title]);
 
     print($layout);
