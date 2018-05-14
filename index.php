b169cc07818ccd0acf7ad3ebbc599a6b3a1f7bbe
Artem Altigin <aaltigin@gmail.com>
2018-05-14 15:09:38 +0300

    Убрал коменты

--- index.php
+++ index.php
@@ -9,10 +9,3 @@
     $layout = generate_template('layout', ['categories' => $categories, 'tasks' => $tasks, 'content' => $content, 'title' => $title]);
 
     print($layout);
-// В одном дне 86400 секунд
-// date_default_timezone_set('Europe/Moscow');
-
-// $datetime1 = time();
-// $end_ts = strtotime("30 May 2018");
-
-// print($end_ts);
