46b29e56f71a1a6217df1d3bc9fbdd07cf363aed
Artem Altigin <aaltigin@gmail.com>
2018-05-22 16:53:04 +0300

    Рефакторинг

--- functions.php
+++ functions.php
@@ -1,7 +1,7 @@
 <?php
     /**
-     * @param array $category - массив категорий задач
-     * @param array $array_task - массив задач
+     * @param array $category массив категорий задач
+     * @param array $array_task массив задач
      */
     function task_counter($category, $array_task) {
         if ($category['project_name'] === 'Все') {
@@ -17,14 +17,14 @@
     };
 
     /**
-     * @param string $date - текстовое представление даты
+     * @param string $date текстовое представление даты
      */
     function date_conversions($date) {
         return date('d.m.Y', strtotime($date));
     };
 
     /**
-     * @param string $input - текст названия новой задачи
+     * @param string $input текст названия новой задачи
      * @return srting
      */
     function input_validation($input) {
@@ -32,8 +32,8 @@
     };
 
     /**
-     * @param string $template_name - название шаблона
-     * @param array $data - данные шаблона
+     * @param string $template_name название шаблона
+     * @param array $data данные шаблона
      * @return string
      */
     function generate_template(string $template_name, array $data) {
@@ -48,7 +48,7 @@
     };
 
     /**
-     * @param string $task_date - дата выполнения задачи
+     * @param string $task_date дата выполнения задачи
      * @param boolean true если до дедлайна осталось 24 часа или меньше
      */
     function set_deadline(string $task_date) {
@@ -66,8 +66,8 @@
     };
 
     /**
-     * @param array $db_param - параметры подключения к базе данных
-     * @param string $query - sql запрос на получение данных из бд
+     * @param array $db_param параметры подключения к базе данных
+     * @param string $query sql запрос на получение данных из бд
      */
     function db_query($db_param, $query)  {
         $db_coonect = mysqli_connect($db_param['address'], $db_param['login'],
@@ -84,26 +84,28 @@
     }
 
     /**
-     * @return string - возвращает строку запроса для получения пользователей
+     * @return string возвращает строку запроса для получения пользователей
      */
     function get_users() {
         return "SELECT `user_id`, `user_name` FROM `users`";
     };
 
     /**
-     * @param string - $id - id пользователя для которого нужно получить данные
-     * @return string - возвращает строку запроса для получения данных пользователя
+     * @param mixed $id пользователя для которого нужно получить данные
+     * @return string возвращает строку запроса для получения данных пользователя
      */
     function get_tasks_by_user($id) {
+        intval($id);
         return "SELECT tasks.task_name, tasks.finish_date, tasks.deadline_date,
         tasks.project_id FROM tasks WHERE tasks.user_id = '$id'";
     };
 
     /**
-     * @param string - $id - id пользователя для которого нужно получить данные
-     * @return string - возвращает строку запроса для получения данных пользователя
+     * @param mixed $id пользователя для которого нужно получить данные
+     * @return string возвращает строку запроса для получения данных пользователя
      */
     function get_categories_by_user($id) {
+        intval($id);
         return "SELECT projects.project_id, projects.project_name FROM projects WHERE projects.user_id = '$id'";
     };
 
